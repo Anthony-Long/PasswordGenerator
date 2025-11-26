fa3be2ca5201c52d11ec464252bc4c3feb5c3068
Anthony Long <anthonyleelong@gmail.com>
2025-11-26 14:34:20 -0500

    finish base password generation

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-console.log(characters)
+const passwordGenerateBtn = document.getElementById('password-generate-btn')
+
+
+passwordGenerateBtn.addEventListener('click', function() {
+    let password1 = returnPassword(characters, 15)
+    let password2 = returnPassword(characters, 15)
+    document.getElementById('result1').textContent = password1
+    document.getElementById('result2').textContent = password2
+   
+})
+
+
+function returnPassword(chars, count) {
+    // Interate through desired count value and grab random indexes from array, push them to final string
+    let password = ''
+    for (let i = 0; i < count; i++) {
+        let randomIndex = Math.floor(Math.random() * chars.length)
+        password += chars[randomIndex]
+    }
+    return password
+}
+
+console.log(returnPassword(characters, 15))
